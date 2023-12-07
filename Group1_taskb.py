62c46b780213dd8a3725650e34a035537b0f9efc
moahelgesson <152410365+moahelgesson@users.noreply.github.com>
2023-12-07 14:43:55 +0100

    cleaning

--- Group1_taskb.py
+++ Group1_taskb.py
@@ -6,7 +6,6 @@ Created on Thu Dec  7 10:33:01 2023
 @author: Moa
 """
 
-
 from gurobipy import *
 import numpy as np
 import math
@@ -16,7 +15,7 @@ import matplotlib.pyplot as plt
 
 #============================================MODEL DATA============================================
 
-with open("data_small.txt", "r") as f:  # Read the data file
+with open("data_small.txt", "r") as f:      # Read the data file
     data = f.readlines()                       
 
 VRP = []                                    # Create array for data related to nodes
@@ -33,22 +32,20 @@ nodes=np.vstack((VRP, VRP[:1]))             # matrix data nodes: customers + sta
 N_row=nodes[:,0]                            # vector of nodes id: customers + start/end depot
 n=len(N_row)                                # Number of nodes
 
-
-K=3                                   # Number of vehicles
+K=3                                         # Number of vehicles
 V=range(K)                                  # Set of vehicles
 N=range (len (N_row))                       # Set of nodes   
-C=range(1,len(N))
-
+C=range(1,len(N))                           # Set of customers
 
 xc=nodes[:,1]                               # X-position of nodes
 yc=nodes[:,2]                               # Y-position of nodes
-a=nodes[:,3]                                # demand 
+a=nodes[:,3]                                # demand of nodes
 r=nodes[:,4]                                # ready time of windows
 d=nodes[:,5]                                # due time of windows
-s=nodes[:,6]                                # service times at customers
+s=nodes[:,6]                                # service times at nodes
 
 b=(130, 130, 130)                           # vehicle capacities
-M=100000                                  # big M
+M=3000                                      # big M
 
 # Create array for euclidian distances between nodes - c(i,j)
 c=np.zeros((n,n))    
@@ -56,38 +53,33 @@ for i in N:
     for j in N:
         c[i][j]=math.sqrt((xc[j] - xc[i])**2 + (yc[j] - yc[i])**2) # Store distance between nodes
             
-
 #========================================OPTIMIZATION MODEL========================================
-
 
 ## Create optimization model
 m = Model('VRPmodel')
 
 ## Create Decision Variables
 
-#arc travel - if  arc(i,j) is travelled by vehicle v it is 1, 0 otherwise
+#arc travel - if vehicle v is travelling arc(i,j) it is 1, 0 otherwise
 x = {}
 for i in N:
     for j in N:
         for v in V:  
             x[i,j,v] = m.addVar(vtype=GRB.BINARY, lb = 0, name="X_%s,%s,%s" %(i,j,v))
             
-# arrival time of vehicle v at customer i
+# start time of service at customer i by vehicle v 
 t = {}
 for j in N:
     for v in V:
         t[j,v] = m.addVar(vtype=GRB.CONTINUOUS, lb = 0, name="T_%s,%s" %(j,v))
 
-
 ## Objective - total distance traveled, i.e., total cost
 obj = (quicksum(c[i,j]*x[i,j,v] for i in N for j in N for v in V))
 m.setObjective(obj, GRB.MINIMIZE)
 
-
 ## Constraints    
 
-
-# All customers visited exactly once
+# All customers visited exactly once, except for depot
 for i in C:
     m.addConstr(quicksum(x[i,j,v] for j in N if i != j if j!=0 for v in V )==1, 'conA[' + str(i) + ']-')
     
@@ -95,19 +87,18 @@ for i in C:
 for v in V:
     m.addConstr(quicksum(a[i]*x[i,j,v] for i in C for j in N if i != j if j!=0) <= b[v] , 'conE[' +  str(v) + ']-')
     
-# All vehicles leave depot (once)
+# All vehicles leave depot exactly once
 for v in V:
     m.addConstr(quicksum(x[0,j,v] for j in N if j!=0 )==1, 'conB[' +  str(v) + ']-')      
 
-# Incoming and outcoming 
+# Incoming and outcoming arc
 for h in C:
     for v in V:
          m.addConstr(quicksum(x[i,h,v] for i in N if h !=i ) == quicksum(x[h,j,v] for j in N if h!= j  ), 'conC[' + str(h) + ',' + str(v) + ']-')    
 
-# All vehicles return to depot (once)
+# All vehicles return to depot exactly once
 for v in V:
-    m.addConstr(quicksum(x[i,20,v] for i in C) == 1, 'conD[' + str(v) + ']-')  
-
+    m.addConstr(quicksum(x[i,n-1,v] for i in C) == 1, 'conD[' + str(v) + ']-')  
 
 # Time window - part 1
 for j in N:
@@ -125,14 +116,12 @@ for i in N:
         for v in V:
             if j!=i:
                 if j!=0:
-                    if i!=20:
+                    if i!=n-1:
                         m.addConstr(t[i,v] + c[i,j] + s[i] - M*(1-x[i,j,v]) <= t[j,v], 'conH[' + str(i) + ',' + str(j) + ',' + str(v) + ']-') 
-
      
-
 m.update()
-m.write('VRPmodel.lp')
-m.Params.timeLimit = 1800 #time limit so optimization will stop after 3600 seconds 
+# m.write('VRPmodel.lp')
+m.Params.timeLimit = 1800 #time limit so optimization will stop after 1000 seconds 
 m.optimize()
 
 # Plot the routes that are decided to be travelled 
